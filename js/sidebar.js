32471cbd801f41ff30c1c6979250358f27993e99
JK6373 <hwang6373@gmail.com>
2026-02-05 20:05:07 +0800

    49

--- js/sidebar.js
+++ js/sidebar.js
@@ -185,8 +185,6 @@
         if (panel) panel.classList.remove('show');
         if (overlayEl) overlayEl.classList.remove('show');
     }
-    window.toggleNotificationPanel = toggleNotificationPanel;
-    window.closeNotificationPanel = closeNotificationPanel;
 
     function escapeHtml(text) {
         if (text == null) return '';
@@ -275,6 +273,8 @@
     window.handleLogout = handleLogout;
     window.toggleLanguageDropdown = toggleLanguageDropdown;
     window.selectLanguage = selectLanguage;
+    window.toggleNotificationPanel = toggleNotificationPanel;
+    window.closeNotificationPanel = closeNotificationPanel;
     window.updateExpirationCountdown = updateExpirationCountdown;
 
     function init() {
