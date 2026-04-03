a79d267528330d6d40772122aa190c87c37c2e72
kunzzit01 <kunzzit01@gmail.com>
2026-04-03 15:02:53 +0800

    28

--- dashboard.php
+++ dashboard.php
@@ -228,14 +228,14 @@ $canViewAnalytics = ($role === 'admin'); // 只有admin可以查看分析
                         </div>
                     </div>
                     <!-- Company Buttons -->
-                    <div id="company-buttons-wrapper" class="transaction-company-filter">
+                    <div id="company-buttons-wrapper" class="transaction-company-filter" style="display: none;">
                         <span class="transaction-company-label">Company:</span>
                         <div id="company-buttons-container" class="transaction-company-buttons">
                             <!-- Company buttons will be dynamically added here -->
                         </div>
                     </div>
                     <!-- Currency Buttons (below Company) -->
-                    <div id="currency-buttons-wrapper" class="transaction-company-filter">
+                    <div id="currency-buttons-wrapper" class="transaction-company-filter" style="display: none;">
                         <span class="transaction-company-label">Currency:</span>
                         <div id="currency-buttons-container" class="transaction-company-buttons">
                             <!-- Currency buttons will be dynamically added here -->
