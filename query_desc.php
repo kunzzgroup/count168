5be9d963aaaa158fc904dba8dea6a739bf2755fd
kunzzit01 <kunzzit01@gmail.com>
2026-04-06 13:45:49 +0800

    56

--- query_desc.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'config.php';
-$stmt = $pdo->query('DESCRIBE company_ownership');
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
--- test3.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'config.php';
-$stmtUser = $pdo->prepare("SELECT CONCAT('U_', id) as id, login_id as account_name, name, role FROM user WHERE status = 'active' AND LOWER(role) = 'owner'");
-$stmtUser->execute();
-print_r($stmtUser->fetchAll(PDO::FETCH_ASSOC));
-?>
