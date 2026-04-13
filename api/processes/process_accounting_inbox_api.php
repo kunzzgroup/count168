b732d936e3c860ef9c8c7a94ac6e537779d68210
JS <kunzzit02@gmail.com>
2026-04-13 14:55:59 +0800

    (79.8)

--- api/processes/process_accounting_inbox_api.php
+++ api/processes/process_accounting_inbox_api.php
@@ -639,7 +639,7 @@ try {
     }
 
     //$today = date('Y-m-d');
-    $today = '2026-05-13';
+    $today = '2026-06-13';
 
     $hasFrequency = hasBankProcessFrequencyColumn($pdo);
     $hasIssueFlagColumn = tableHasColumn($pdo, 'bank_process', 'issue_flag');
