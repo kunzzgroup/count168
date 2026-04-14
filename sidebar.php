6cca12819dcaee16326acb4e018cb60bf2d2f6b5
kunzzit01 <kunzzit01@gmail.com>
2026-04-14 11:20:59 +0800

    336

--- sidebar.php
+++ sidebar.php
@@ -522,13 +522,11 @@ if ($companyId) {
                                         <span>Data Capture</span>
                                     </a>
                                 <?php endif; ?>
-                                <?php /* Transaction link hidden – Maintenance shows only Payment & Process
-                                <?php if (!$isCurrentCompanyC168): ?>
+                                <?php if (!$isCurrentCompanyC168 && $companyHasGambling && $hasMaintenance): ?>
                                     <a href="transaction_maintenance.php" class="submenu-item" id="maintenance-transaction-link">
                                         <span>Transaction</span>
                                     </a>
                                 <?php endif; ?>
-                                */ ?>
                                 <?php if ($hasMaintenance): ?>
                                     <a href="payment_maintenance.php" class="submenu-item">
                                         <span>Payment</span>
