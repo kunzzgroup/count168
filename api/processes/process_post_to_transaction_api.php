28a85c2469299789d4a69cbde147c8b2ac5e8b99
JS <kunzzit02@gmail.com>
2026-04-17 09:58:12 +0800

    (186)

--- api/processes/process_post_to_transaction_api.php
+++ api/processes/process_post_to_transaction_api.php
@@ -1194,7 +1194,7 @@ try {
             $txn['account_id'] = (int) $p['customer_id'];
             $txn['amount'] = round($price, 2);
             $txn['description'] = $isManualInactiveCompensation
-                ? ("Inactive Compensation Sell Price" . round($price, 2))
+                ? "Inactive Compensation Sell Price"
                 : ("Process: Sell Price for $processLabel" . $suffix);
             insertTransactionRow($pdo, $txn);
             $createdCount++;
