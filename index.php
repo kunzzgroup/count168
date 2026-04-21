9bca3be8776afaeac3b064f2f581624cf5f3cc7a
kunzzit01 <kunzzit01@gmail.com>
2026-04-21 16:56:31 +0800

    515

--- index.php
+++ index.php
@@ -71,6 +71,7 @@ if (isset($_COOKIE['remember_token'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EazyCount</title>
+    <link rel="icon" type="image/png" href="images/count_logo.png">
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>" />
     <link rel="stylesheet" href="css/index.css?v=<?php echo time(); ?>" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
