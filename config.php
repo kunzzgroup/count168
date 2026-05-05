1cd2b2dc4e84a44f1a5e81fe8afd6166774e0d38
JS <kunzzit02@gmail.com>
2026-05-05 15:01:15 +0800

    43

--- config.php
+++ config.php
@@ -4,18 +4,6 @@ $dbname = 'u857194726_count168_site';
 $dbuser = 'u857194726_count168_site';
 $dbpass = 'Kunzz_c168';
 
-$dbname = 'u857194726_Games';
-$dbuser = 'u857194726_Games';
-$dbpass = 'Kunzz_c168';
-
-$dbname = 'u857194726_Banks';
-$dbuser = 'u857194726_Banks';
-$dbpass = 'Kunzz_c168';   
-
-$dbname = 'u857194726_C168';
-$dbuser = 'u857194726_C168';
-$dbpass = 'Kunzz_c168'; 
-
 // 设置PHP时区为马来西亚时间
 date_default_timezone_set('Asia/Kuala_Lumpur');
 
