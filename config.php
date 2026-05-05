598ef89a2cc254f52fc7e344aa34638ac7217a20
JS <kunzzit02@gmail.com>
2026-05-05 13:40:44 +0800

    42

--- config.php
+++ config.php
@@ -12,6 +12,10 @@ $dbname = 'u857194726_Banks';
 $dbuser = 'u857194726_Banks';
 $dbpass = 'Kunzz_c168';   
 
+$dbname = 'u857194726_C168';
+$dbuser = 'u857194726_C168';
+$dbpass = 'Kunzz_c168'; 
+
 // 设置PHP时区为马来西亚时间
 date_default_timezone_set('Asia/Kuala_Lumpur');
 
