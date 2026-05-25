387a2fecf4f45e29f19b4d8ac00add1d7fb984c5
kunzzit01 <kunzzit01@gmail.com>
2026-05-25 12:04:34 +0800

    90

--- frontend/src/pages/datacapture/hooks/useDataCapturePaste.js
+++ frontend/src/pages/datacapture/hooks/useDataCapturePaste.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 import {
   parseCitibetFormatBasedPaste,
   parseCitibetMajorPaymentReport,
