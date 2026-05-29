ccbb2e8236ae881b97a4b6e367849f591a28adfa
JS <kunzzit02@gmail.com>
2026-05-29 11:03:54 +0800

    207.2.0.1

--- frontend/dist/assets/usePartnershipAuditWriteGuard-BjhVI6pb.js
+++
@@ -1,1 +0,0 @@
-import{bm as i,r as o,cD as c}from"./index-Boa6JBvW.js";function s(t,r,a){const u=i(t),e="Read-only account: this action is not allowed.",n=o.useCallback(()=>c(t,()=>{typeof r=="function"&&r(a||e,"danger")}),[t,r,a]);return{mutationsBlocked:u,guardWrite:n,isLocked:u}}export{s as u};
