b8b07c448f720f5804fb8ad8d0efc11caed5b990
JS <kunzzit02@gmail.com>
2026-06-02 15:15:13 +0800

    220.24.1

--- frontend/dist/assets/usePartnershipAuditWriteGuard-Ce6dr0r6.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:frontend/dist/assets/usePartnershipAuditWriteGuard-CpezwQ5v.js
-import{r as i}from"./index-DRfdR9-N.js";import{u as n,g as d}from"./partnershipAuditReadOnly-bIKN5qTl.js";function p(t,r,a){const u=n(t),o="Read-only account: this action is not allowed.",e=i.useCallback(()=>d(t,()=>{typeof r=="function"&&r(a||o,"danger")}),[t,r,a]);return{mutationsBlocked:u,guardWrite:e,isLocked:u}}export{p as u};
-========
-import{r as i}from"./index-DMZ7ESxD.js";import{u as n,g as d}from"./partnershipAuditReadOnly-DWnaIbcC.js";function p(t,r,a){const u=n(t),o="Read-only account: this action is not allowed.",e=i.useCallback(()=>d(t,()=>{typeof r=="function"&&r(a||o,"danger")}),[t,r,a]);return{mutationsBlocked:u,guardWrite:e,isLocked:u}}export{p as u};
->>>>>>>> bda91a48 (220.24.1):frontend/dist/assets/usePartnershipAuditWriteGuard-Ce6dr0r6.js
