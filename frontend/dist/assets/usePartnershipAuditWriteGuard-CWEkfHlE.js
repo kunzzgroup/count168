fb4a0ec9961a5531768c175b28e9109bbd61439b
JS <kunzzit02@gmail.com>
2026-06-03 09:51:51 +0800

    220.48.1

---
+++ frontend/dist/assets/usePartnershipAuditWriteGuard-CWEkfHlE.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-wOSa3jxg.js";import{u as n,g as d}from"./partnershipAuditReadOnly-Cum-g0D4.js";function p(t,r,a){const u=n(t),o="Read-only account: this action is not allowed.",e=i.useCallback(()=>d(t,()=>{typeof r=="function"&&r(a||o,"danger")}),[t,r,a]);return{mutationsBlocked:u,guardWrite:e,isLocked:u}}export{p as u};
