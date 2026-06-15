7645906504e084d210470b8b40320e9c6beed9d4
kunzzit01 <kunzzit01@gmail.com>
2026-06-15 16:08:06 +0800

    69

--- frontend/src/pages/report/customer/CustomerReportPage.jsx
+++ frontend/src/pages/report/customer/CustomerReportPage.jsx
@@ -32,7 +32,6 @@ import "../../../../public/css/transaction.css";
 import "../../../../public/css/userlist.css";
 import "../../../../public/css/customer_report.css";
 import "../../../../public/css/report-outlined-fields.css";
-import "../../../../public/css/maintenance_unified_filters.css";
 import "../../../../public/css/date-range-picker.css";
 import "../../../../public/css/maintenance_notifications.css";
 import { fetchAccounts, fetchCustomerReport } from "./customerReportApi.js";
