3cd71cf7b70a6a2a67f3e8b5b50baa4cab51ec76
kunzzit01 <kunzzit01@gmail.com>
2026-06-16 15:18:15 +0800

    120

--- frontend/dist/assets/usePartnershipAuditWriteGuard-Co1L4ol_.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./index-c8gOW2NT.js";import{u as n,g as d}from"./partnershipAuditReadOnly-oPqWo368.js";function p(t,r,a){const u=n(t),o="Read-only account: this action is not allowed.",e=i.useCallback(()=>d(t,()=>{typeof r=="function"&&r(a||o,"danger")}),[t,r,a]);return{mutationsBlocked:u,guardWrite:e,isLocked:u}}export{p as u};
