840efc8f3ad67ff31eecf1c61e3eb54cca87212e
Jasonlol63 <jasonlewzc@gmail.com>
2026-06-21 16:37:12 +0800

    87.9.0.5

--- api/processes/process_accounting_inbox_api.php
+++ api/processes/process_accounting_inbox_api.php
@@ -1165,8 +1165,8 @@ try {
         exit;
     }
 
-    //$today = date('Y-m-d');
-    $today = '2026-06-22';
+    $today = date('Y-m-d');
+    //$today = '2026-06-22';
 
     $hasFrequency = hasBankProcessFrequencyColumn($pdo);
     $hasIssueFlagColumn = tableHasColumn($pdo, 'bank_process', 'issue_flag');
