86169a0bbd687ebd824b0d310999781ce9581eb7
kunzzit01 <kunzzit01@gmail.com>
2026-06-23 10:54:24 +0800

    153

--- frontend/src/pages/domain/components/DomainFormModal.jsx
+++ frontend/src/pages/domain/components/DomainFormModal.jsx
@@ -24,8 +24,8 @@ import { getDomainText } from "../../../translateFile/pages/domainTranslate.js";
 import DomainModalPortal from "./DomainModalPortal.jsx";
 import ConfirmDeleteModal, { CONFIRM_DELETE_NESTED_Z_INDEX } from "../../../components/ConfirmDeleteModal.jsx";
 
-/** 与 AuthenticatedLayout TABLET_MEDIA_QUERY 一致 */
-const DFM_COMPACT_LAYOUT_MQ = "(max-width: 1280px)";
+/** 与 domain 列表页 tablet 断点一致（含 iPad Pro 12.9" 横屏 1366px） */
+const DFM_COMPACT_LAYOUT_MQ = "(max-width: 1366px)";
 
 function useDomainFormCompactLayout() {
   const [compact, setCompact] = useState(() => {
