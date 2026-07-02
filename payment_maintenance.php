127ab89596dd027b0e2f734a92fe74307cb21703
Jk6373 <hwang6373@gmail.com>
2026-07-02 09:38:46 +0800

    2

--- payment_maintenance.php
+++ payment_maintenance.php
@@ -91,7 +91,7 @@ if (!empty($session_company_id)) {
                         <svg class="search-icon" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
                         </svg>
-                        <input type="text" id="filter_search" placeholder="e.g. UP017 / AG102" class="search-input maintenance-search-input" autocomplete="off">
+                        <input type="text" id="filter_search" placeholder="Account, description, submitter..." class="search-input maintenance-search-input" autocomplete="off">
                     </div>
                 </div>
                 
