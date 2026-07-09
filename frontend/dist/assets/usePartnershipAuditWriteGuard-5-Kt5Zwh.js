aa5bd846328980073699739492a0b1444314f7bc
kunzzit01 <kunzzit01@gmail.com>
2026-07-09 16:05:05 +0800

    281

--- frontend/dist/assets/usePartnershipAuditWriteGuard-5-Kt5Zwh.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:frontend/dist/assets/usePartnershipAuditWriteGuard-DDNMNnBh.js
-import{r as i}from"./index-8OOVD_FW.js";import{u as n,g as d}from"./partnershipAuditReadOnly-BqNGkz0q.js";function p(t,r,a){const u=n(t),o="Read-only account: this action is not allowed.",e=i.useCallback(()=>d(t,()=>{typeof r=="function"&&r(a||o,"danger")}),[t,r,a]);return{mutationsBlocked:u,guardWrite:e,isLocked:u}}export{p as u};
-========
-import{r as i}from"./index-D5f2acbI.js";import{u as n,g as d}from"./partnershipAuditReadOnly-DpEczjKu.js";function p(t,r,a){const u=n(t),o="Read-only account: this action is not allowed.",e=i.useCallback(()=>d(t,()=>{typeof r=="function"&&r(a||o,"danger")}),[t,r,a]);return{mutationsBlocked:u,guardWrite:e,isLocked:u}}export{p as u};
->>>>>>>> d5f4d56e650a7b5405341751c216231972ced616:frontend/dist/assets/usePartnershipAuditWriteGuard-5-Kt5Zwh.js
